af84afe86792e331afa61bd41313934296582c69
Tihomir <tihomir.j.marinov@gmail.com>
2022-02-12 01:11:37 +0200

    map

--- server/src/middlewares/auth-guard.middleware.ts
+++ server/src/middlewares/auth-guard.middleware.ts
@@ -12,7 +12,7 @@ export function authGuard(req: Request, res: Response, next: NextFunction) {
     try {
         token = req.headers.authorization?.split(' ')[1];
         if (!token) {
-            return next(new HttpError('Auth failed', 401))
+            return next(new HttpError('auth failed', 401))
         }
         const privateKey = process.env.JWT_PRIVATE_KEY as string;
         const decodedToken: any = verify(token, privateKey);
