8c9309848f62ab67551754a2b347c847d859432b
Tihomir <tihomir.j.marinov@gmail.com>
2022-02-12 00:35:04 +0200

    map

--- client/src/components/spots/Map.tsx
+++ client/src/components/spots/Map.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = (props: any) => <div
-    style={ { width: '50px', height: '20px', backgroundColor: 'red' } }>{ props.text }</div>;
 const Map = (props: any) => {
     const apiKey = process.env.REACT_APP_GOOGLE_API_KEY || 'AIzaSyDlmZ0PvW0yBwB1b-cTX-fTjVfUQUUqWXY'
     return (
@@ -11,10 +9,7 @@ const Map = (props: any) => {
             defaultCenter={ props.center }
             defaultZoom={ props.zoom }
         >
-            <AnyReactComponent
-                { ...props.center }
-                text="My Marker"
-            />
+        {/*    Markers*/}
         </GoogleMapReact>
     );
 };
