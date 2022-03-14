ed8575eea7b8cce7c964e4c4ab69a57749de65c3
Tihomir <tihomir.j.marinov@gmail.com>
2022-03-14 15:50:31 +0200

    add side menu test

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,32 +1,52 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, userSelector } from '../../state/userSlice';
-import FormButton from '../form/FormButton';
+import styled from 'styled-components';
+import { BaseButton } from '../form/input/StyledButtons';
+import { toggleSideMenu } from '../../state/appSlice';
 
+
+const HeaderWrapper = styled.header`
+  height: 70px;
+  background-color: #171717;
+  color: #fff;
+  display: flex;
+  width: 100%;
+  align-items: baseline;
+
+`
+const MessageWrapper = styled.span`
+  align-self: center;
+  font-size: 18px;
+  width: 100%;
+`
+
+
+const SignOutButton = styled(BaseButton)`
+  height: 36px;
+  width: 100px;
+  margin-right: 10px;
+  align-self: center;
+  font-size: 16px;
+`
 
 const Header = () => {
-    const { id, name } = useSelector(userSelector);
+    const { id, name, avatar } = useSelector(userSelector);
     const dispatch = useDispatch();
-    const signOutButtonProps = {
-        text: 'Sign Out',
-        className: 'sign-out',
-        onClick: () => {
-            dispatch(logout())
-        }
+    const logoutUser = () => {
+        dispatch(logout())
     }
-
-
+    const openSideMenu = () => {
+        dispatch(toggleSideMenu())
+    }
     return (
-        <div className="header">
-            <div className="welcome-message">
-                <span>{ name ? `Hello ${ name }` : 'Welcome' }</span>
+        <HeaderWrapper>
+            <MessageWrapper>{ name ? `Hello, ${ name }` : 'Welcome' }
+            </MessageWrapper>
+            <div>
+                <span onClick={ openSideMenu }>burger</span>
             </div>
-            { id ?
-                <div className="sign-out-wrapper">
-                    <FormButton { ...signOutButtonProps }/>
-                </div>
-                : null }
-        </div>
+        </HeaderWrapper>
     );
 };
 
--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { AppPaths } from '../../common/constants/routes';
-import { userSelector } from '../../state/userSlice';
 import SpotList from '../spots/SpotList';
 import Header from '../header/Header';
-
+import SideMenu from './SideMenu';
 
 
 const HomePage = () => {
-    const { avatar } = useSelector(userSelector)
     return (
         <div>
             <Header/>
-            <img style={ { width: '200px', height: '200px' } } src={ avatar }/>
             <Link to={ AppPaths.EditUser }>Edit Profile</Link>
             <SpotList/>
-
+            <SideMenu/>
         </div>
     );
 };
