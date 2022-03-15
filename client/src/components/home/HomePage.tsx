46678f4a329fc0309c5686b9c37083bce0d25b7b
Tihomir <tihomir.j.marinov@gmail.com>
2022-03-15 16:19:41 +0200

    refactoring

--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppPaths } from '../../common/constants/routes';
 import SpotList from '../spots/SpotList';
 
 
 
 const HomePage = () => {
     return (
-        <div>
-            <Link to={ AppPaths.EditUser }>Edit Profile</Link>
+        <>
             <SpotList/>
-        </div>
+        </>
     );
 };
 
