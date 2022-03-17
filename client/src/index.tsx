3d6d29fbd6abba1ba93ac3b7d9d044e1c6a98297
Tihomir <tihomir.j.marinov@gmail.com>
2022-03-17 16:05:26 +0200

    change api keys

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.scss';
-import { Provider } from 'react-redux';
-import store from './state/Store';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.scss";
+import { Provider } from "react-redux";
+import store from "./state/Store";
 
 ReactDOM.render(
-    <Provider store={ store }>
-        <App/>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
