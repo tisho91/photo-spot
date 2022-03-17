02fd5bab0f3ce00696aa224105ae0bf6e35b00fa
Tihomir <tihomir.j.marinov@gmail.com>
2022-03-17 18:00:40 +0200

    test dev deploy

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import router from "./src/routes";
 import { json } from "body-parser";
-import path, { join, resolve } from "path";
+import { join, resolve } from "path";
 import { dbConfig } from "./src/config/db.config";
 import { corsMiddleware } from "./src/middlewares/cors.middleware";
 import { HttpError } from "./src/utils/http-error";
@@ -15,13 +15,9 @@ if (process.env.NODE_ENV !== "production") {
 
 app.use(express.static(join(__dirname, "../../client/build")));
 app.use(router);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
-
-  app.get("*", (req: any, res: any) => {
-    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-  });
-}
+app.use((req, res, next) => {
+  res.sendFile(resolve(__dirname, "../../client/build", "index.html"));
+});
 app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
     return next(error);
